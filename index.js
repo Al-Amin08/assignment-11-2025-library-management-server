9aac7cc371294d45a1dfac4bf4de93ca603ebe11
Al-Amin08 <mehadislam08@gmail.com>
2025-04-21 20:47:57 +0600

    add delete api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -85,6 +85,18 @@ async function run() {
 
         })
 
+        app.post('/return-books/:id', async (req, res) => {
+            // const id = req.params.id
+            const { bookId } = req.body
+            console.log(bookId)
+            const query = { _id: new ObjectId(id) }
+            const result = await borrowedBookCollection.deleteOne(query)
+            res.send({ success: true, result })
+
+            await categoryWiseBooksCollection.updateOne({ bookId: bookId }, { $inc: { quantity: 1 } })
+
+            res.send({ success: true })
+        })
 
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
